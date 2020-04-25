06d610a94e71fdaac9ecf942376ac6fc0cce2b1b
Jules LE BRIS <jls.lebris@gmail.com>
2020-04-25 18:13:45 +0200

    :sparkles: add pwa

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,8 +1,25 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
+  plugins: [
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: true
+      }
+    ]
+  ],
   head: [
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "icons/logo128.png"}],
+    ['link', { rel: 'icon', href: '/icons/logo128.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/logo128.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/logo128.png', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/logo128.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     nav: [
